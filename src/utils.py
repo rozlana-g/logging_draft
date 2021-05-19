bcc852beaea369825216e54cd331d51e5d43f767
Rozlana <rozlanka@gmail.com>
2021-05-19 16:39:36 +0300

    не повторяю одни и те же операции на всех логгерах, а только один раз на main, от которого наследуются остальные.

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,21 @@
 import logging
-import pathlib
 import sys
 
-from src.config import APP_LOGGER_NAME, LOGGING_LEVEL
+from src.config import LOGGING_LEVEL
+
+logger = logging.getLogger('src')
+logger.setLevel(LOGGING_LEVEL)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(module)s - %(funcName)s - "
+                              "%(lineno)d - %(levelname)s - %(message)s")
+sh = logging.StreamHandler(sys.stdout)
+sh.setFormatter(formatter)
+logger.handlers.clear()  # строчка не нужна если я только в одном месте делаю setup_applevel_logger(logger_name=APP_LOGGER_NAME)?
+logger.addHandler(sh)
 
 
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.propagate = False
-    logger.setLevel(LOGGING_LEVEL)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(module)s - %(funcName)s - "
-                                  "%(lineno)d - %(levelname)s - %(message)s")
-    sh = logging.StreamHandler(sys.stdout)
-    sh.setFormatter(formatter)
-    logger.handlers.clear()  # строчка не нужна если я только в одном месте делаю setup_applevel_logger(logger_name=APP_LOGGER_NAME)?
-    logger.addHandler(sh)
 
     # file with current logs
     fh = logging.FileHandler(f"logs/{logger.name}.log", mode='w')
