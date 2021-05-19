00f7a4af26fa3b048a4bc11d82984c248b67b083
Rozlana <rozlanka@gmail.com>
2021-05-19 17:04:57 +0300

    не повторяю одни и те же операции при каждом импорте utils.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,18 @@
-from src import utils
-from src import module
+import logging
+import sys
 
-logger = utils.get_logger('src')
+from src.config import LOGGING_LEVEL, log_formatter
+
+logger = logging.getLogger('src')
+logger.setLevel(LOGGING_LEVEL)
+sh = logging.StreamHandler(sys.stdout)
+sh.setFormatter(log_formatter)
+logger.handlers.clear()  # строчка не нужна если я только в одном месте делаю setup_applevel_logger(logger_name=APP_LOGGER_NAME)?
+logger.addHandler(sh)
+
 logger.info('------------------------- NEW RUN --------------------------------------')
+
+from src import module
 
 logger.debug('Calling module function.')
 try:
--- src/utils.py
+++ src/utils.py
@@ -1,29 +1,25 @@
 import logging
 import sys
 
-from src.config import LOGGING_LEVEL
-
-logger = logging.getLogger('src')
-logger.setLevel(LOGGING_LEVEL)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(module)s - %(funcName)s - "
-                              "%(lineno)d - %(levelname)s - %(message)s")
-sh = logging.StreamHandler(sys.stdout)
-sh.setFormatter(formatter)
-logger.handlers.clear()  # строчка не нужна если я только в одном месте делаю setup_applevel_logger(logger_name=APP_LOGGER_NAME)?
-logger.addHandler(sh)
+from src.config import log_formatter
 
 
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.propagate = False
 
+    sh = logging.StreamHandler(sys.stdout) # потому что handler-ы не наследуются от логгеров-родителей
+    sh.setFormatter(log_formatter)
+    logger.addHandler(sh)
+
     # file with current logs
-    fh = logging.FileHandler(f"logs/{logger.name}.log", mode='w')
-    fh.setFormatter(formatter)
+    fh = logging.FileHandler(f"logs/{logger.name}.log", mode='w') # потому что handler-ы не наследуются от логгеров-родителей,
+                                                                    # да и название файла было бы другое
+    fh.setFormatter(log_formatter)
     logger.addHandler(fh)
 
     # file with all the historical logs
     fh = logging.FileHandler(f"history_logs/{logger.name}.log", mode='a')
-    fh.setFormatter(formatter)
+    fh.setFormatter(log_formatter)
     logger.addHandler(fh)
     return logger
